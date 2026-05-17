7affaa40f454d5a9acd6f891bbed341dc276addf
hoyeonjo01 <hoyeonjo0522@gmail.com>
2026-05-17 20:47:42 +0900

    update

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 const icons = document.querySelector(".icons");
-const korean = document.querySelector(".korean");
-const english = document.querySelector(".english");
 const loadingScreen = document.querySelector(".loading-screen");
+const video = document.querySelector(".loading-video");
 
 setTimeout(() => {
   icons.classList.add("hide");
 }, 1600);
 
 setTimeout(() => {
-  korean.classList.add("show");
-}, 2100);
+  video.classList.add("show");
+  video.currentTime = 0;
+  video.play();
+}, 1900);
 
-setTimeout(() => {
-  korean.classList.add("hide");
-}, 3300);
+video.addEventListener("ended", () => {
 
-setTimeout(() => {
-  english.classList.add("show");
-}, 3700);
+  setTimeout(() => {
+    loadingScreen.classList.add("fade-out");
+  }, 3000);
 
-setTimeout(() => {
-  loadingScreen.classList.add("fade-out");
-}, 5200);
+  setTimeout(() => {
+    window.location.href = "main.html";
+  }, 4500);
 
-setTimeout(() => {
-  window.location.href = "main.html";
-}, 6200);
+});
